4a4cd76362cd96bd76fa89d961932515f45fdddc
notBigO <2003varunp@gmail.com>
2024-08-07 18:29:57 +0530

    chore: Update app.ts to use express middleware for JSON and URL-encoded body parsing

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express, { type Express } from "express";
 const app: Express = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
